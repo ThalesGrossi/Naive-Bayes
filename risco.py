f58a68bb4749fe60e62c17de24d0f8b067978ed5
Thales Grossi <thales.grossi@gmail.com>
2025-11-17 09:27:49 -0300

    Pré-processamento dos dados

--- risco.py
+++ risco.py
@@ -1,2 +1,25 @@
-import pandas as pandas
+import pickle
+
+import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+# Importando a base de dados
+base_risco_credito = pd.read_csv('risco_credito.csv')
+
+# Separação em preditores e classes
+x_risco_credito = base_risco_credito.iloc[:, 0:4].values
+y_risco_credito = base_risco_credito.iloc[:, 4].values
+
+label_encoder_historia = LabelEncoder()
+label_encoder_divida = LabelEncoder()
+label_encoder_garantias = LabelEncoder()
+label_encoder_renda = LabelEncoder()
+
+x_risco_credito[:, 0] = label_encoder_historia.fit_transform(x_risco_credito[:, 0])
+x_risco_credito[:, 1] = label_encoder_divida.fit_transform(x_risco_credito[:, 1])
+x_risco_credito[:, 2] = label_encoder_garantias.fit_transform(x_risco_credito[:, 2])
+x_risco_credito[:, 3] = label_encoder_renda.fit_transform(x_risco_credito[:, 3])
+
+with open('risco_credito.pkl', 'wb') as f:
+    pickle.dump([x_risco_credito, y_risco_credito], f)
